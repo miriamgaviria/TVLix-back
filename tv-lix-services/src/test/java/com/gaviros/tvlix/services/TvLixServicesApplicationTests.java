3af6733ad18511560fb4d297ac549519f66becad
miriam-gaviria2 <52879306+miriam-gaviria2@users.noreply.github.com>
2020-03-02 20:21:39 +0100

    Tablas creadas y primer controlador completo

---
+++ tv-lix-services/src/test/java/com/gaviros/tvlix/services/TvLixServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaviros.tvlix.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvLixServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
